a7756b5cc7aeb655618d3c7c106e2cf9433e559e
nluizsoliveira <nluizsoliveira@gmail.com>
2024-09-23 14:37:39 -0300

    feat: import rendered version of old blog

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+app.listen(8000);
+
+const publicFolder = path.join(__dirname, 'public');
+
+app.use('/', express.static(publicFolder))
+app.get('/',
+    function(req, res){
+        res.sendFile(
+            path.join(publicFolder, 'about.html')
+        )
+    }
+)
+
+app.get('/about',
+    function(req, res){
+        res.sendFile(
+            path.join(publicFolder, 'about.html')
+        )
+    }
+)
+
+app.get('/blog',
+    function(req, res){
+        res.sendFile(
+            path.join(publicFolder, 'blog.html')
+        )
+    }
+)
+
+app.get('/resume',
+    function(req, res){
+        res.sendFile(
+            path.join(publicFolder, 'resume.html')
+        )
+    }
+)
+
+app.get('/experience',
+    function(req, res){
+        res.sendFile(
+            path.join(publicFolder, 'experience.html')
+        )
+    }
+)
+
+app.get('/projects',
+    function(req, res){
+        res.sendFile(
+            path.join(publicFolder, 'projects.html')
+        )
+    }
+)
