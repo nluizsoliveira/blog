e090e049d7b9c23b3fae0700030348652a809905
nluizsoliveira <nluizsoliveira@gmail.com>
2024-09-25 10:29:13 -0300

    fix: fix door

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express();
-app.listen(8000);
+app.listen(8080);
 
 const publicFolder = path.join(__dirname, 'public');
 
